7e9ebee85ae1a1045570ac617bf3c0d023389fe8
danil <minakonn9shkoni@gmail.com>
2021-09-27 12:06:21 +0300

    end lesson

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// console.log(1);
-// console.log(undefined);
+let Firstnumber = 45;
+let Twonumb = 12;
 
-//variable
+console.log(Firstnumber + Twonumb);
+console.log(Firstnumber - Twonumb);
+console.log(Firstnumber / Twonumb);
+console.log(Firstnumber * Twonumb);
+console.log(Firstnumber % Twonumb);
 
-let num1;
-//инициализация
-num1 = 33;
-//логирование
-console.log(num1);
+let numbTwo = 101;
+console.log(numbTwo);
+let TorF = true;
+console.log(TorF);
+let java = "Java";
+console.log(java);
+let nameUser = "Elon musk";
+console.log(nameUser);
 
-const num3 = 77;
-console.log(num3)
+let number = 23;
+number+=11
+console.log(number);
+
