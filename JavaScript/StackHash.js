cd06c2a2bdde79499cafb32c63ecddb8a53118ea
danil <minakonn9shkoni@gmail.com>
2021-10-21 12:02:35 +0300

    Add Queue

--- JavaScript/StackHash.js
+++ JavaScript/StackHash.js
@@ -83,3 +83,30 @@ const checkSequence = (str, options)=>{
   return stack.isEmpty;
 }
 console.log(checkSequence('()()()', options));
+
+
+class Queue{
+  constructor(...args){
+    this._head = 0;
+    this._tail = 0;
+    for(const argument of args){
+      this.push(argument);
+    }
+  }
+  get size(){
+    return this._tail-this._head;
+  }
+  push(value){
+    this[this._tail]=value;
+    this._tail++;
+    return this.size;
+  }
+  unShift(){
+    if(this.size){
+      const deItem = this[this._head];
+      delete this[this._head++];
+      return deItem;
+    }
+  }
+}
+
