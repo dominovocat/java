cee1bc71ae03969e8d7271dc6332d8ea49171c15
danil <minakonn9shkoni@gmail.com>
2021-10-24 16:22:06 +0300

    Add example iterator(spread)

--- JavaScript/map.js
+++ JavaScript/map.js
@@ -20,16 +20,17 @@ dictionary.set('небо','sky');
 
 const str = 'Собака огонь смотреть ребёнок этаж взять кот';
 
-const translate = (str)=>{
-  const lowerCaseStr = str.toLowerCase();
-  const wordsArray = lowerCaseStr.split(' ');
-  const wordsTranslatedArray = wordsArray.map((word)=>{
-    if(dictionary.has(word)){
-      return dictionary.get(word);
-    }
-    return word;
-  });
-  const translatedStr = wordsTranslatedArray.join(' ');
-  return translatedStr;
+const translate = (dictionary,str,separator = ' ')=>{
+  return str.toLowerCase()
+    .split(separator)
+    .map((word)=> dictionary.has(word) ? dictionary.get(word):word)
+    .join(separator);
+}    
+console.log(translate(dictionary,str));
+
+
+const keysArray = [];
+for (const iterator of dictionary.keys() ) {
+  keysArray.push(iterator)
 }
-console.log(translate(str))
+const valuesArray = [...dictionary.values()];
